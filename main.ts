6613de9e7bbb3c46a6d84e2909b16072e91ab27b
Jet-Lu888 <jet.lu@stu.ocsb.ca>
2026-02-13 12:44:02 -0500

    Changed world to MTHS and changed comment.

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Jet Lu
+ * Created on: Feb 2026
+ * This program will show "Hello, MTHS"
 */
 
-basic.showString('Hello, World!')
+basic.showString('Hello, MTHS!')
